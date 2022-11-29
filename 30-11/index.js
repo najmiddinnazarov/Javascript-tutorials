565b563d32149cfd700d5dfb18a5e212ea8a4fdb
najmiddinnazarov <najmiddinnazarov0405@gmail.com>
2022-11-30 04:02:19 +0500

    Numbers and string

---
+++ 30-11/index.js
@@ -0,0 +1,89 @@
+// Numbers
+
+// console.log(10000000);
+// console.log(100_000_000);
+// console.log(1e8); // 1 * 100000000
+// console.log(1e-6);
+// console.log(1e500);
+
+// console.log(0.1 + 0.2 == 0.3);
+
+// let num = 0.1 + 0.2;
+// console.log(Number(num.toFixed(16)));
+
+// let number = 7;
+// console.log(number.toString(2));
+// console.log(parseInt(111, 2));
+
+// console.log(parseFloat(1.4213));
+// console.log(parseInt(1.4213));
+
+// console.log(isNaN("assd"));
+
+// let num = 5.213123123;
+// let num2 = 0.213123123;
+// console.log(num.toPrecision(5));
+// console.log(num2.toPrecision(5));
+
+// Math
+
+// console.log(Math.PI);
+// console.log(Math.abs(3.3));
+// console.log(Math.floor(1.9999));
+// console.log(Math.ceil(1.1));
+// console.log(Math.round(1.5));
+// console.log(Math.trunc(99.432423));
+// console.log(Math.trunc(Math.random() * 10));
+// console.log(Math.min(30, 3, 2, 5, 8, 3));
+// console.log(Math.max(30, 3, 2, 5, 8, 3));
+// console.log(Math.pow(20, 5));
+// console.log(Math.sign(-0)); // 1, -1, 0
+
+// String
+
+// let str1 = String(1);
+// let str2 = new String();
+// let str3 = "";
+// console.log(str1, str2, str3);
+
+// console.log("sada\nsd");
+// console.log(`sadasd
+// asdasd123`);
+
+// let text = "Hello world, welcome to the universe.";
+// let result = text.indexOf("welcome");
+// let result2 = text.indexOf("e", 5);
+// console.log(result);
+// console.log(result2);
+
+// let text = "Hello";
+// console.log(text.length);
+// console.log(text.toUpperCase());
+// console.log(text.toLowerCase());
+
+// let str = "Hello";
+// console.log(str.padEnd(12, "!"));
+// console.log(str.padStart(12, "!"));
+
+// let text = "Hello world!";
+// let result = text.slice(3, 5);
+// console.log(result);
+
+// console.log(eval("1 + 2"));
+
+// let str = "          sdasd sadas sdad          ";
+// console.log(str.trim());
+// console.log(str.trimEnd());
+
+// let str = "sdasd asdas";
+// console.log(str.split(" "));
+
+// let conc1 = "1sadsa";
+// let conc2 = "2sadsa";
+// let conc3 = "3sadsa";
+// console.log(conc2.concat(conc1));
+
+// let arr = ["sa", "sad", "sad"];
+// for (const iterator of arr) {
+//   console.log(iterator);
+// }
