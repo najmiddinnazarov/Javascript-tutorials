a2f7e06c6882ed1b9ef7259fda38b2481ebcb870
najmiddinnazarov <najmiddinnazarov0405@gmail.com>
2022-12-21 16:55:04 +0500

    new Map, new Set, weakMap, weakSet

---
+++ 12-12/index.js
@@ -0,0 +1,56 @@
+// new Map, new Set, weakMap, weakSet
+// const obj = {
+//   name: "Najmiddin",
+//   1: "number",
+//   [undefined]: "undefined",
+//   null: "null",
+// };
+// const map = new Map();
+// map.set("name", "Najmiddin");
+// map.set(null, null);
+// // map.delete(null);
+// // map.clear();
+// // console.log(map.size);
+// console.log(map.has("name"));
+// console.log(map.has("surName"));
+
+// let recipeMap = new Map([
+//   ["cucumber", 500],
+//   ["tomatoes", 350],
+//   ["onion", 50],
+// ]);
+// console.log(recipeMap);
+
+// for (let vegetable of recipeMap.keys()) {
+//   alert(vegetable);
+// }
+
+// for (let amount of recipeMap.values()) {
+//   alert(amount);
+// }
+
+// for (let entry of recipeMap) {
+//   alert(entry);
+// }
+
+// recipeMap.forEach((value, key, map) => {
+//   alert(`${key}: ${value} => ${map}`);
+// });
+
+// let set = new Set();
+
+// let john = { name: "John" };
+// let pete = { name: "Pete" };
+// let mary = { name: "Mary" };
+
+// set.add(john);
+// set.add(pete);
+// set.add(mary);
+// set.add(john);
+// set.add(mary);
+
+// // console.log(set.size);
+
+// for (let user of set) {
+//   alert(user.name);
+// }
