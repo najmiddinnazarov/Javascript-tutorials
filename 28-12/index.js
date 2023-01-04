cc5b4ff773e1dbdd0146b75d22172548f5361844
najmiddinnazarov <najmiddinnazarov0405@gmail.com>
2023-01-05 03:26:04 +0500

    DOM basics

---
+++ 28-12/index.js
@@ -0,0 +1,27 @@
+// function sayHi() {
+//   console.log("Hello");
+// }
+
+// window.sayHi();
+
+// console.log(window.innerHeight); // inner window height
+// console.log(window.innerWidth); // inner window width
+
+// document.body.style.backgroundColor = "black";
+// console.log(document.documentElement);
+// console.log(document.head);
+// console.log(document.body);
+
+// const id = document.getElementById("id");
+// const class1 = document.getElementsByClassName("class");
+// const attr = document.getElementsByTagName("span");
+// const name = document.getElementsByName("name");
+// console.log(id, class1, attr, name);
+
+// const getClass = document.querySelector(".class");
+// const getId = document.querySelector("#id");
+// const getTag = document.querySelector("span");
+// const getDiv = document.querySelectorAll("div");
+// // console.log(getClass, getId, getTag);
+// let txt = getClass.textContent("bb");
+// console.log(txt);
